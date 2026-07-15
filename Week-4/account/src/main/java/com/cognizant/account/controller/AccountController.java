92fcbfcfc77c85576eae8e3f132bc96bc25d8728
Visva1216 <visvaprasanthn@gmail.com>
2026-07-15 18:58:38 +0530

    Week4

---
+++ Week-4/account/src/main/java/com/cognizant/account/controller/AccountController.java
@@ -0,0 +1,20 @@
+package com.cognizant.account.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import com.cognizant.account.model.Account;
+
+@RestController
+public class AccountController {
+
+	@GetMapping("/accounts/{number}")
+	public Account getAccountByNumber(@PathVariable String number) {
+		// Dummy response without backend connectivity
+		Account account = new Account();
+		account.setNumber("00987987973432");
+		account.setType("savings");
+		account.setBalance(234343);
+		return account;
+	}
+}
